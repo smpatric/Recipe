ac3448948955397a73c13195c8b8671a0572aa92
smpatric <patrick.cody.stewart@gmail.com>
2015-02-21 13:17:02 -0500

    Recipe Commit

---
+++ RecipeScript.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+	$("#shopHead").click(function(){
+		$("#shop").slideToggle("slow"),
+		$("#shopHead").toggleClass("clicked");
+	});
+	$("#prepHead").click(function(){
+		$("#prep").slideToggle("slow"),
+		$("#prepHead").toggleClass("clicked");
+	});
+	$("#cookHead").click(function(){
+		$("#cook").slideToggle("slow"),
+		$("#cookHead").toggleClass("clicked");
+	});
+	$("ul > li").click(function(){
+		$(this).css("text-decoration", "line-through");
+	});
+	$(".footer").click(function(){
+		$("#pics").slideToggle("slow"),
+		$("#instructions").css("padding-top", "1em");
+	});
+});
